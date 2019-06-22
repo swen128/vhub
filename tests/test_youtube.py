b293106184bb5e465e64e2a8025f35960714bb24
PND <fujjisaaan@gmail.com>
2019-06-22 11:58:47 +0900

    Add test cases for the function `get_video_by_id`.

--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -1,6 +1,21 @@
+import json
+import sys
 import unittest
 from fetch_video import parse_videos_list
-from youtube import is_valid_youtube_video_url
+from youtube import is_valid_youtube_video_url, YouTube
+
+sys.path.append('lib')
+
+from lib.googleapiclient.http import HttpMockSequence
+
+
+def read_file(path: str) -> str:
+    with open(path, 'r') as f:
+        return f.read()
+
+
+api_discovery = read_file("tests/api_responses/youtube/discovery.json")
+PLACE_HOLDER = ""
 
 
 class TestIsValidYoutubeVideoUrl(unittest.TestCase):
@@ -19,3 +34,32 @@ class TestIsValidYoutubeVideoUrl(unittest.TestCase):
     def test_invalid_url(self):
         url = "https://www.example.com/"
         self.assertFalse(is_valid_youtube_video_url(url))
+
+
+class TestGetVideoById(unittest.TestCase):
+    def test_found(self):
+        id = "03H1qSot9_s"
+        response = read_file("tests/api_responses/youtube/videos/list/no_mention.json")
+        http = HttpMockSequence([
+            ({"status": 200}, api_discovery),
+            ({"status": 200}, response)
+        ])
+        youtube = YouTube(secret=PLACE_HOLDER, http=http)
+        
+        out = youtube.get_video_by_id(id)
+        ground_truth = json.loads(response)['items'][0]
+
+        self.assertEqual(out['id'], id)
+        self.assertDictEqual(out, ground_truth)
+    
+    def test_not_found(self):
+        id = PLACE_HOLDER
+        response = read_file("tests/api_responses/youtube/videos/list/not_found.json")
+        http = HttpMockSequence([
+            ({"status": 200}, api_discovery),
+            ({"status": 200}, response)
+        ])
+        youtube = YouTube(secret=PLACE_HOLDER, http=http)
+
+        with self.assertRaises(ValueError):
+            youtube.get_video_by_id(id)
