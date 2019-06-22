e11fba26c632d35c2a225a8ac778a6509da26d90
PND <fujjisaaan@gmail.com>
2019-06-22 11:16:44 +0900

    Add a test case for the function `is_valid_youtube_video_url`.

--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -15,3 +15,7 @@ class TestIsValidYoutubeVideoUrl(unittest.TestCase):
     def test_hyphen_containing_id(self):
         url = "https://www.youtube.com/watch?v=-_xY7wNvw9k"
         self.assertTrue(is_valid_youtube_video_url(url))
+
+    def test_invalid_url(self):
+        url = "https://www.example.com/"
+        self.assertFalse(is_valid_youtube_video_url(url))
